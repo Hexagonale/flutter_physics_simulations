d99ebe9f8fae4d4b011fa38151eb544ee3c57f0a
Przemysław Kędzierski <przemyslaw@cdot.app>
2022-09-10 11:33:39 +0200

    Add object reference to the state and derivative

--- lib/utils/rk4/rk4_solver.dart
+++ lib/utils/rk4/rk4_solver.dart
@@ -7,20 +7,20 @@ class Rk4Solver {
   /// [function] should return list of derivatives for each of the simulation objects.
   /// [initialState] should contain initial states for each of the simulation objects.
   /// [delta] should be seconds since last simulation frame.
-  List<ObjectState<T>> solve<T extends Vector>({
-    required List<ObjectDerivative<T>> Function(List<ObjectState<T>> state) function,
-    required List<ObjectState<T>> initialState,
+  List<ObjectState<T, R>> solve<T extends Vector, R>({
+    required List<ObjectDerivative<T, R>> Function(List<ObjectState<T, R>> state) function,
+    required List<ObjectState<T, R>> initialState,
     required double delta,
   }) {
-    final List<ObjectDerivative<T>> k1 = function(initialState);
-    final List<ObjectDerivative<T>> k2 = function(initialState + k1.toState(delta * 0.5));
-    final List<ObjectDerivative<T>> k3 = function(initialState + k2.toState(delta * 0.5));
-    final List<ObjectDerivative<T>> k4 = function(initialState + k3.toState(delta));
+    final List<ObjectDerivative<T, R>> k1 = function(initialState);
+    final List<ObjectDerivative<T, R>> k2 = function(initialState + k1.toState(delta * 0.5));
+    final List<ObjectDerivative<T, R>> k3 = function(initialState + k2.toState(delta * 0.5));
+    final List<ObjectDerivative<T, R>> k4 = function(initialState + k3.toState(delta));
 
-    final List<ObjectState<T>> newStates = <ObjectState<T>>[];
+    final List<ObjectState<T, R>> newStates = <ObjectState<T, R>>[];
     for (int i = 0; i < initialState.length; i++) {
-      final ObjectDerivative<T> sum = k1[i] + k2[i] * 2 + k3[i] * 2 + k4[i];
-      final ObjectState<T> change = sum.toState(delta / 6);
+      final ObjectDerivative<T, R> sum = k1[i] + k2[i] * 2 + k3[i] * 2 + k4[i];
+      final ObjectState<T, R> change = sum.toState(delta / 6);
 
       newStates.add(initialState[i] + change);
     }
