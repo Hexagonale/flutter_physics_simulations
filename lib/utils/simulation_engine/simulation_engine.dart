e39861e00413becae53a0525f7211cd96bf47b97
Przemysław Kędzierski <przemyslaw@cdot.app>
2022-09-10 13:16:49 +0200

    Added doc to the SimulationEngine

--- lib/utils/simulation_engine/simulation_engine.dart
+++ lib/utils/simulation_engine/simulation_engine.dart
@@ -1,17 +1,38 @@
 import 'dart:async';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:physics/physics.dart';
 
+/// Parent class for the all of the physics simulations.
+///
+/// Takes care of setting up physics iteration time [_updateTimer]
+/// with the given [updateFrequency].
+///
+/// Integrates [states] using the given [solver] and [getDerivativesForStates].
+///
+/// [simulationSpeed] can be used to adjust speed of the simulation. By default it is real time.
 abstract class SimulationEngine<T extends Vector, R> {
   SimulationEngine({
+    required this.solver,
     required this.updateFrequency,
+    required this.states,
+    this.simulationSpeed = 1.0,
   });
+
+  final OdeSolver solver;
 
   final Duration updateFrequency;
 
-  late final Timer _updateTimer;
+  List<ObjectState<T, R>> states;
 
+  double simulationSpeed;
+
+  /// Timer that runs every simulation frame.
+  ///
+  /// Initialized in the [init].
+  Timer? _updateTimer;
+
+  /// Takes care of storing the delta time.
   final Stopwatch _stopwatch = Stopwatch();
 
   @mustCallSuper
@@ -26,13 +47,22 @@ abstract class SimulationEngine<T extends Vector, R> {
 
   @mustCallSuper
   void dispose() {
-    _updateTimer.cancel();
+    _updateTimer?.cancel();
 
     _stopwatch.stop();
   }
 
+  /// Called every simulation frame.
   void _update() {
     final double delta = _stopwatch.elapsedMicroseconds / 1000 / 1000;
+
+    preUpdate(delta);
+
+    states = solver.solve(
+      function: getDerivativesForStates,
+      initialState: states,
+      delta: delta,
+    );
 
     update(delta);
 
@@ -40,5 +70,12 @@ abstract class SimulationEngine<T extends Vector, R> {
     _stopwatch.start();
   }
 
-  void update(double delta);
+  /// Called before the [states] are updated.
+  void preUpdate(double delta) {}
+
+  /// Called after the [states] are updated.
+  void update(double delta) {}
+
+  /// See `function` parameter to the [OdeSolver.solve].
+  List<ObjectDerivative<T, R>> getDerivativesForStates(List<ObjectState<T, R>> states);
 }
